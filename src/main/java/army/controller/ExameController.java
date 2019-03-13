965230685b342ba15e4e1ed628574a165c928f6d
chendan <1224394878@qq.com>
2019-03-13 10:55:11 +0800

    exam commit

--- src/main/java/army/controller/ExameController.java
+++ src/main/java/army/controller/ExameController.java
@@ -1,5 +1,42 @@
 package army.controller;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import army.db.pojo.Question;
+import army.db.pojo.User;
+import army.service.ExameService;
+import utils.MD5Utils;
+import utils.ResponseCode;
+import utils.ServerResponse;
+import utils.TimeUntils;
+
+@Controller
+@RequestMapping("/exam")
 public class ExameController {
+	@Autowired
+	private ExameService exameService;
+
+	// 添加题目
+	@RequestMapping("addQuestion.do")
+	@ResponseBody
+	public ServerResponse addQuestion(HttpServletRequest request, HttpServletResponse response, Question question,
+			Model model) {
+		ServerResponse serverResponse;
+		if (exameService.insertQuestion(question)) {
+			serverResponse = new ServerResponse(ResponseCode.SUCCESS.getCode(), "题目添加成功");
+		} else {
+			serverResponse = new ServerResponse(ResponseCode.ERROR.getCode(), "题目添加失败");
+		}
+		return serverResponse;
+	}
 
 }
--- src/main/java/army/service/ExameService.java
+++ src/main/java/army/service/ExameService.java
@@ -1,5 +1,18 @@
 package army.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import army.db.dao.QuestionMapper;
+import army.db.pojo.Question;
+
+@Service
 public class ExameService {
+	@Autowired
+	private QuestionMapper questionMapper;
+
+	public boolean insertQuestion(Question question) {
+		return questionMapper.insert(question) == 1 ? true : false;
+	}
 
 }
